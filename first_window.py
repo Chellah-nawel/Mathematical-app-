e9323863372b4f595088d40f0e7f695080d46a9f
Chellah-nawel <chellah.nawel@gmail.com>
2026-05-03 18:30:35 +0100

    creating the left frame

--- first_window.py
+++ first_window.py
@@ -1,2 +1,14 @@
 import tkinter 
 import customtkinter
+
+app=customtkinter.CTk()
+app.geometry("1000x500")
+app.title("First Window")
+app._set_appearance_mode("light")
+BlueFrame= customtkinter.CTkFrame(master=app,
+                                  height= 900,
+                                  width= 200,
+                                  fg_color="darkblue")
+#put the frame in the let side of the window
+BlueFrame.pack(pady=0, padx=0, side="left")
+app.mainloop()
