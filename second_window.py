905978a868d6a3b6ea85c86751deec7919505ee7
Chellah-nawel <chellah.nawel@gmail.com>
2026-05-05 23:16:37 +0100

    creation du header

--- second_window.py
+++ second_window.py
@@ -18,3 +18,32 @@ win.geometry("1080x700")
 win.title("Axe 1 — Fonctions non linéaires")
 win.configure(fg_color=LIGHT_BG)
 win._set_appearance_mode("light")
+
+#create the top
+header = customtkinter.CTkFrame(win, 
+                                fg_color=WHITE, 
+                                corner_radius=0, 
+                                height=54)
+header.pack(fill="x")
+header.pack_propagate(False)
+
+#btn retour arrière
+retour= customtkinter.CTkButton( header, 
+                        text="←", 
+                        width=38, 
+                        height=38,
+                        fg_color="transparent", 
+                        text_color=DARK, 
+                        hover_color="#f0f0f0",
+                        font=customtkinter.CTkFont(size=20), 
+                        corner_radius=8
+)
+retour.pack(side="left", padx=(12, 6), pady=8)
+
+#title
+title= customtkinter.CTkLabel(header, 
+                       text="Axe 1 : Fonctions non linéaires",
+                        font=customtkinter.CTkFont(size=17, weight="bold"), 
+                        text_color=DARK
+)
+title.pack(side="left")
