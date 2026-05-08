39059b2e5008142037cecee023f153e8b773393e
Chellah-nawel <chellah.nawel@gmail.com>
2026-05-08 23:01:00 +0100

    ajouter la fonction de descente de gradien

--- descente_de_gradient/algo_descente_gradient.py
+++ descente_de_gradient/algo_descente_gradient.py
@@ -10,3 +10,38 @@ def gradient(f, variables):
         result.append(derive)
 
     return np.array(result)
+
+
+# descente de gradient
+def gradient_descent(f, variables, start, alpha, tolerance):
+    i=0
+
+    grad = gradient(f, variables)
+
+    # convertir f en fct numerique 
+    grad_func = []
+    for i in grad:
+        grad_func.append(lambdify(variables, i))
+
+    # les x0
+    x = np.array(start, dtype=float)
+
+    while (i<100):
+
+        # valeur du gradient au point actuel
+        grad_value = []
+        for i in grad_func:
+            grad_value.append(i(*x)) # * pour depiler le tableau
+
+        grad_value = np.array(grad_value, dtype=float)
+
+        xn = x - alpha * grad_value
+
+        #condition d'arret
+        if np.linalg.norm(xn - x) < tolerance:
+            break
+
+        x=xn
+        i+=1
+
+    return x
